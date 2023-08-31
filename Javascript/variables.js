4ea591255d06dcfeeece6c22707c27052db1ff29
zahid-soft <zahidsarang95@gmail.com>
2023-08-31 16:06:38 +0530

    let const and var ki khani

---
+++ Javascript/variables.js
@@ -0,0 +1,10 @@
+// let const and var ki khani
+
+const accountId = 124343;
+let accountEmail = "zahid@google.com";
+var accountPassword = "zahid123";
+accountCity = "Mumbai";
+
+let accountState=null;
+console.log(typeof(accountState))
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
