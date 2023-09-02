360d7da6c5465c4b04831de31b97998f8a5898c6
zahid-soft <zahidsarang95@gmail.com>
2023-09-02 13:57:28 +0530

    conversion of data types

---
+++ Javascript/conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33abc";
+
+console.log(typeof score);
+console.log(typeof score);
+
+let numberInValue = Number(score);
+console.log(typeof numberInValue);
+console.log(numberInValue);
+
+// => when convert varible into number
+// 33 => 33
+// "33" => 33
+// "33anv" => NaN
+// true => 1
+// null => 0
+// false => 0
+// undefined => undefined
+
+//================================================================//
+// let isbol = undefined
+// let isbolValue = Boolean(isbol)
+// console.log(isbolValue)
+// => when convert varible into boolean
+// 1=> true
+// 0=> false
+// "" => false
+// "zahid" => true
+// undefined => false
