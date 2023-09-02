4794fe780b53fb4967c48fdebc68ca757ba68842
zahid-soft <zahidsarang95@gmail.com>
2023-09-02 13:25:45 +0530

    datatypes

---
+++ Javascript/dataTypes.js
@@ -0,0 +1,22 @@
+"user strict"; // treat all js code as newer versions
+
+// alert(3+3) // it will give a error because we are using nodejs, not browser
+
+// Primitive data types
+/*
+* number: => range should be in 2 to power of 53
+* bigint: => for large numbers
+* string: 
+* boolean:
+* null: => standalone value
+* undefined: 
+* symbol:
+*/
+
+// Non primitive data types
+
+/*
+* object: 
+* array:
+*/
+
