7ef7ba41efa937629208c937006bb10c69e5c59e
zahid-soft <zahidsarang95@gmail.com>
2023-09-08 20:51:54 +0530

    maths and number

---
+++ Javascript/number_and_maths.js
@@ -0,0 +1,32 @@
+// const score = 400;
+// const blance = new Number(100);
+// console.log(score);
+// console.log(blance);
+// console.log(blance.toString().length);
+// console.log(blance.toFixed(2));
+
+// const otherNumber = 23.87293
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hunders = 10000000
+
+// console.log(hunders.toLocaleString('en-In'));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++ //
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(40.9));
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const Min = 10;
+const Max = 20;
+
+const number = Math.floor(Math.random() * (Max - Min + 1) + Min);
+console.log(number);
