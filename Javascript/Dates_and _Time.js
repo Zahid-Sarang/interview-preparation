0ea6f9bc77438eb1e7b5ceef8c67eced5de83ba5
zahid-soft <zahidsarang95@gmail.com>
2023-09-09 14:16:56 +0530

    arrays

---
+++ Javascript/Dates_and _Time.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.getMonth() +1);
+console.log(myDate.getDay());
+// console.log(myDate.toString())
+// console.log(myDate.toJSON())
+/* console.log(myDate.toLocaleString('default',{
+   weekday: "long",
+   timeZone: "Europe"
+}));
+*/
+// console.log(myDate.toDateString())
+
+// let myCradtedDate = new Date("01-14-2023");
+// console.log(myCradtedDate);
+// console.log(myCradtedDate.toString());
+// console.log(myCradtedDate.toLocaleString());
+// console.log(myCradtedDate.toDateString());
+// console.log(myCradtedDate.getTime());
+// console.log(Math.floor(myCradtedDate.getTime()) / 1000);
