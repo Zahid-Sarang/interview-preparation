e598210cd55c448a801392342cf79082e75b94ed
zahid-soft <zahidsarang95@gmail.com>
2023-09-10 20:12:27 +0530

    arrays

--- TypeScript/objects/objects.ts
+++ TypeScript/objects/objects.ts
@@ -17,16 +17,79 @@
 
 // ================================================================= Type Aliases =================================================================
 
+// type User = {
+// 	name: string;
+// 	email: string;
+// 	isActive: boolean;
+// };
+
+// function createUser(user: User): User {
+// 	return { name: "Zah", email: "zah@gmail", isActive: true };
+// }
+
+// const result = createUser({ name: "", email: "", isActive: false});
+// console.log(result);
+
+// ================================================================= Read Only =================================================================
+
+// const User = {
+//     name: "hitesh",
+//     email: "hitesh@lco.dev",
+//     isAvtive: true
+// }
+
+// function createUser({name: string, isPaid: boolean}){}
+
+// let newUser = {name: "hitesh", isPaid: false, email: "h@h.com"}
+
+// createUser(newUser)
+
+// function createCourse():{name: string, price: number}{
+//     return {name: "reactjs", price: 399}
+// }
+
+// type User = {
+//     name: string;
+//     email: string;
+//     isActive: boolean
+// }
+
+// function createUser(user: User): User{
+//     return {name: "", email: "", isActive: true}
+// }
+
+// createUser({name: "", email: "", isActive: true})
+
 type User = {
+	readonly _id: string;
 	name: string;
 	email: string;
 	isActive: boolean;
+	credcardDetails?: number;
 };
 
-function createUser(user: User): User {
-	return { name: "Zah", email: "zah@gmail", isActive: true };
-}
+let myUser: User = {
+	_id: "1245",
+	name: "h",
+	email: "h@h.com",
+	isActive: false,
+};
 
-const result = createUser({ name: "", email: "", isActive: false});
-console.log(result);
+type cardNumber = {
+	cardnumber: string;
+};
+
+type cardDate = {
+	cardDate: string;
+};
+
+type cardDetails = cardNumber &
+	cardDate & {
+		cvv: number;
+	};
+
+myUser.email = "h@gmail.com";
+// myUser._id = "asa"
+
 export {};
+
