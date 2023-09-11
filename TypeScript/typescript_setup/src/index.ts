37ab978ce508b4c9a4edf51acc92a4c6e62b4795
zahid-soft <zahidsarang95@gmail.com>
2023-09-11 13:02:39 +0530

    generic and class access modifier

--- TypeScript/typescript_setup/src/index.ts
+++ TypeScript/typescript_setup/src/index.ts
@@ -1,11 +1,53 @@
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+
+//     }
+// }
 class User {
-	email: string;
-	name: string;
-   readonly city: string ="khargahr"
-	constructor(email: string, name: string) {
-		this.email = email;
-		this.name = name;
-	}
+
+    protected _courseCount = 1
+    
+    readonly city: string = "Jaipur"
+    constructor(
+        public email: string, 
+        public name: string,
+        // private userId: string
+        ){
+    }
+    private deleteToken(){
+        console.log("Token deleted");
+        
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
 }
 
-const zahid = new User("zahid@gmail.com", "Zahid");
+class SubUser extends User {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
+
+const hitesh = new User("h@h.com","hitesh")
+// hitesh.name 
+
+// hitesh.deleteToken()
