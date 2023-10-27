0e9a35e488f37e718e1c09ffe65c2a64067164bf
zahid-soft <zahidsarang95@gmail.com>
2023-10-27 20:54:06 +0530

    indireact-recursion

--- DSA/advance-DSA/indirect-recursion.js
+++ DSA/advance-DSA/indirect-recursion.js
@@ -18,11 +18,11 @@
 
 // perctice question
 
-function test(x) {
+function recursion(x) {
 	if (x > 0) {
-		test(x - 1);
+		recursion(x - 1);
 	}
 	console.log(x);
 }
 let data = 5;
-test(data);
+recursion(data);
