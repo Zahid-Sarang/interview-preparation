58054f02e8aa90dc88d98e1e334a6460f0f08678
zahid-soft <zahidsarang95@gmail.com>
2023-10-27 19:49:09 +0530

    reverse array using recursion

---
+++ DSA/advance-DSA/reverse-array-recursion.js
@@ -0,0 +1,19 @@
+// let data = [3, 4, 2, 54, 12];
+// let temp;
+
+// function customeReverse(data, start, end) {
+// 	console.log(data);
+// 	if (start <= end) {
+// 		temp = data[start];
+// 		data[start] = data[end];
+// 		data[end] = temp;
+// 		customeReverse(data, start + 1, end - 1);
+// 	}
+// }
+
+// customeReverse(data, 0, data.length - 1);
+
+let item = [1, 2, 3, 4, 5];
+const result = item.reverse();
+
+console.log("using rever function", result);
